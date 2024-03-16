a9576b3810faab12d4a6fe5ff33506323d6b335e
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-03-16 11:27:07 +0000

    connect

--- frontend/components/partials/Header/index.tsx
+++ frontend/components/partials/Header/index.tsx
@@ -84,10 +84,7 @@ const Header: React.FC = (): React.ReactElement => {
                 </div>
 
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  <DynamicWidget innerButtonComponent="Connect with Dynamic"></DynamicWidget>
-                </div>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  {/* <DynamicWidget innerButtonComponent="Connect with Worldcoin"></DynamicWidget> */}
+                  <DynamicWidget innerButtonComponent="Connect"></DynamicWidget>
                 </div>
               </div>
             </div>
