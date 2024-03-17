b521dda2806208975e5cd97635cf6343c14844b8
Walid Sabihi <walid.sabihi@cs.ox.ac.uk>
2024-03-17 05:42:05 +0000

    feat(fe): Extract addresses to env file

--- frontend/constants/contracts.js
+++ frontend/constants/contracts.js
@@ -1,8 +1,8 @@
 import onlyContract from '../../hardhat-starter-kit/build/artifacts/contracts/OnlyToken.sol/OnlyToken.json'
 import marketplaceContract from '../../hardhat-starter-kit/build/artifacts/contracts/Marketplace.sol/Marketplace.json'
-export const onlyContract_ADDRESS = "0x5B557183636e4b72F05721036F3655af5885f282"
+export const onlyContract_ADDRESS = process.env.NEXT_PUBLIC_ONLYCONTRACT_ADDRESS
 export const onlyContract_ABI = onlyContract.abi;
 
-export const marketplaceContract_ADDRESS = "0x1EBb2B623bd01d2ce4907799cf853f1d95411114"
+export const marketplaceContract_ADDRESS = process.env.NEXT_PUBLIC_MARKETPLACECONTRACT_ADDRESS
 export const marketplaceContract_ABI = marketplaceContract.abi;
 export const chain_ID = 421614;
