b82b9df7dbf4d7629c18ab9c39dfa6c94f0aaf37
vrebol <vid.rebol11@gmail.com>
2024-03-17 02:35:41 +0100

    Added brand profile

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -11,7 +11,8 @@ import { Fragment, useEffect, useState } from 'react';
 import {useApi} from "../hooks/useApi";
 import { IDKitWidget, VerificationLevel, ISuccessResult } from '@worldcoin/idkit'
 import ProfileKeyVal  from './profilekeyval'
-
+import InfluencerProfile from './influencer-profile'
+import BrandProfile from './brand-profile'
 
 
 const UPLOAD_IMAGE_PATH = process.env.NEXT_PUBLIC_SUPABASE_UPLOAD_IMAGE_PATH;
@@ -36,6 +37,21 @@ const Profile = () => {
     image: "https://pbs.twimg.com/profile_images/708090673208033280/_RcBYLyg_400x400.jpg"
   });
 
+  const [brandData, setbrandData] = useState({
+    username: "ethglobal",
+    name:"EthGlobal",
+    email: "example@gmail.com",
+    wallet: "nfewisndgprbsfuoidkbuewops32768",
+    website: "ethglobal.com",
+    location: "London",
+    description: "Building our decentralized future",
+    size: 100,
+
+    //phone: 40392439479, we dont do phone right
+    image: "https://pbs.twimg.com/profile_images/708090673208033280/_RcBYLyg_400x400.jpg"
+  });
+
+  const [brand, setBrand] = useState(true);
   // useEffect(() => {
   //   if (!dynamicContext.isAuthenticated) {
   //     void router.push('/login');
@@ -51,11 +67,11 @@ const Profile = () => {
   //
   // }, [dynamicContext.isAuthenticated]);
 
-  const [showWallet, setShowWallet] = useState(false);
+  // const [showWallet, setShowWallet] = useState(false);
 
-  const toggleWallet = () => {
-    setShowWallet(!showWallet);
-  };
+  // const toggleWallet = () => {
+  //   setShowWallet(!showWallet);
+  // };
 
   const handleVerify = async (proof: ISuccessResult) => {
       const res = await fetch("/world-verify", { // route to your backend will depend on implementation
@@ -116,82 +132,12 @@ const Profile = () => {
 
           <Tab.Panels className="flex flex-col items-center justify-center p-5 mt-3 bg-white border rounded-xl">
             <Tab.Panel>
-              <section className="flex flex-col items-center justify-center py-5 space-y-2 text-gray-800">
-                <div className="flex items-end mb-1">
-                  <Avatar avatar={userData?.image} isRounded size="md" />
-
-                  {(
-                    <div className="relative top right-5" title="Certified account">
-                      <CheckCircleIcon className="w-4 h-4 bg-white rounded-full text-primary-500" />
-                    </div>
-                  )}
-                </div>
-
-
-                <div className="flex flex-col items-center justify-center space-y-3">
-                
-                  <div className="flex flex-col items-center justify-center ">  
-                  <h1 className="text-xl font-bold">{userData.name}</h1>
-                  <h3 className="mb-2 ">{userData.follower_count} followers</h3>
-                  </div>
-                  <ProfileKeyVal label="Username" content={userData.username} />
-                  {/* <h3 className="mb-1 text-l justify-center items-center space-x-2">Platform: {userData.platform}</h3> */}
-                  <ProfileKeyVal label="Platform" content={userData.platform} />
-
-                  <ProfileKeyVal label="Industry" content={userData.industry} />
-
-                  {/* <h3 className="mb-1 text-l justify-center items-center space-x-2">Industry: {userData.industry}</h3> */}
-
-                  <ProfileKeyVal label="Sex" content={userData.sex} />
-
-                  <ProfileKeyVal label="Age" content={userData.age} />
-                  
-                  <ProfileKeyVal label="Language" content={userData.language} />
-                  
-                  <div className="flex items-center justify-center space-x-2">
-                    <Link href={`mailto:${userData.email}`} title="Drop me a message">
-                      <EnvelopeIcon className="inline-block w-5 h-5" />
-                    </Link>
-                    {/* {{userData.phone && (
-                      <Link href={`mailto:${userData.phone}`} title="Drop me a message">
-                      <PhoneIcon className="inline-block w-5 h-5" />
-                      </Link>
-                    )}} */}
-                  </div>
-                  <div>
-                    {!showWallet && (
-                      <h3 className="text-primary-700 mb-1 text-l font-semibold cursor-pointer" onClick={toggleWallet}>Wallet address</h3>
-                    )}
-                    {showWallet && (
-                      <h3 className="mb-1 text-l font-semibold" onClick={toggleWallet}>{userData.wallet}</h3>
-                    )}
-                  
-                  </div>
-                  
-
-
-                  {/* {userData.phone && (
-                    <p className="text-sm">
-                      <span className="font-semibold">Phone:</span> {userData.phone}
-                    </p>
-                  )} */}
-
-                  <div>
-                  <IDKitWidget
-                      app_id="app_staging_36f4ed912bf5790caf5fdb754bc5bf3c" // obtained from the Developer Portal
-                      action="sign-in-action" // obtained from the Developer Portal
-                      onSuccess={onSuccess} // callback when the modal is closed
-                      handleVerify={handleVerify} // callback when the proof is received
-                      verification_level={VerificationLevel.Orb}
-                  >
-                      {({ open }) =>
-                      // This is the button that will open the IDKit modal
-                        <button onClick={open}>Connect with World ID</button>
-                      }
-                  </IDKitWidget >
-                  </div>
-                </div>
-              </section>
+            {!brand && (
+              <InfluencerProfile userData={userData} />
+            )}
+            {brand && (
+              <BrandProfile brandData={brandData} />
+            )}
             </Tab.Panel>
 
             <Tab.Panel>
